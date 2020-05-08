63c7ef78c132d8c5b9b35441bd102818326944a7
vihan melaka <vihan97@gmail.com>
2020-05-08 20:34:24 +0530

    slight chnages

--- server.py
+++ server.py
@@ -1,18 +1,38 @@
 import socket
+import time
+import matplotlib.pyplot as plt
+import numpy as np
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind((socket.gethostname(),12301))
+s.bind((socket.gethostname(),1245))
 s.listen(5)
 
+data=[]
+msg=''
+
 while True:
-    msg=''
     clientsocket, address = s.accept()
-    n=0
-    while n<10:
-        msg=msg+clientsocket.recv(10).decode("utf-8")
-        n=n+1
-    print(msg)
+
+    while len(data)!= 900:
+        msg=msg+clientsocket.recv(100).decode("utf-8")
+        data=msg.split(',')
+        if data[len(data)-1]=='':
+            data.pop()
+
+    image=[]
+    temp=[]
+    for each in data:
+        if len(temp)==29:
+            temp.append(int(each))
+            image.append(temp)
+            temp=[]
+        else:
+            temp.append(int(each))
+    print(image)
+    d=np.array(image)
+    plt.gray()
+    plt.imsave("transfer2.jpg",d)
     clientsocket.close()
     #message and the type of data
-    
+
 
